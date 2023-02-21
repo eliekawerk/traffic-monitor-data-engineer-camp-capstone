ab8fa2999feb367c02ef2a54b5f41841861fde69
Luke Huntley <lahuntley@hotmail.com>
2023-02-21 19:10:28 +0800

    Adding awscli pip install

--- data-ingestion/kafka/producer/startup.sh
+++ data-ingestion/kafka/producer/startup.sh
@@ -1,6 +1,8 @@
 # export $(grep -v '^#' .env | xargs)
 # export $(grep -v '^#' .env | sed -e 's/[\r\n]//g')
 # export $(grep -v '^#' aws s3 cp s3://traffic-monitor-env-s3-bucket/.env | sed -e 's/[\r\n]//g')
+# apt install awscli
+python -m pip install awscli
 eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed 's/^/export /')
 echo $server
 echo $username
