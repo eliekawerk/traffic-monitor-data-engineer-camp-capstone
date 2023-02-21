a8c05927fd1fda1f5117b11fac995fd5f3eb25f1
Luke Huntley <lahuntley@hotmail.com>
2023-02-21 19:34:55 +0800

    Remove \r

--- data-ingestion/kafka/producer/startup.sh
+++ data-ingestion/kafka/producer/startup.sh
@@ -2,8 +2,9 @@
 # export $(grep -v '^#' .env | sed -e 's/[\r\n]//g')
 # export $(grep -v '^#' aws s3 cp s3://traffic-monitor-env-s3-bucket/.env | sed -e 's/[\r\n]//g')
 # apt install awscli
+# eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed 's/[^\r]/export /')
 python -m pip install awscli
-eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed 's/^/export /')
+# eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed 's/[^\r]/export /')
 echo $server
 echo $username
 echo $password
