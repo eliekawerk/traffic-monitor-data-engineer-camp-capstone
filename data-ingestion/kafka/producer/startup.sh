4ef65e298a5cbe979fae9b63e503d64a87f94cf9
Luke Huntley <lahuntley@hotmail.com>
2023-02-21 21:00:44 +0800

    adding awscli to requirements file

--- data-ingestion/kafka/producer/startup.sh
+++ data-ingestion/kafka/producer/startup.sh
@@ -1,7 +1,3 @@
-python -m pip install awscli
-#eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed 's/^[\r\n]/export /')
+# python -m pip install awscli
 export eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed -e 's/[\r\n]//g')
-echo $server
-echo $username
-echo $password
 python producer.py -f confluent.config -t traffic-monitor-topic -d 1
