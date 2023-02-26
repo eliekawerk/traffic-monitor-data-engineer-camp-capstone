501652afad15bd77898957ff93f01d3098c7275d
Luke Huntley <lahuntley@hotmail.com>
2023-02-26 22:39:41 +0800

    Additional linitng

--- data-ingestion/kafka/producer/utility/dynamic_faker_providers.py
+++ data-ingestion/kafka/producer/utility/dynamic_faker_providers.py
@@ -2,26 +2,21 @@ from faker.providers import DynamicProvider
 
 # Prepare additional faker providers
 car_colour_provider = DynamicProvider(
-     provider_name="car_colour",
-     elements=["white", "sliver", "black", "red", "blue", "yellow","green"],
+    provider_name="car_colour",
+    elements=["white", "sliver", "black", "red", "blue", "yellow", "green"],
 )
 
 car_fuel_type_provider = DynamicProvider(
-     provider_name="car_fuel_type",
-     elements=["petrol", "diesel", "electric"],
+    provider_name="car_fuel_type",
+    elements=["petrol", "diesel", "electric"],
 )
 
 car_passengers_count_provider = DynamicProvider(
-     provider_name="car_passengers_count",
-     elements=[1, 2, 3, 4]
+    provider_name="car_passengers_count", elements=[1, 2, 3, 4]
 )
 
 car_travel_direction_provider = DynamicProvider(
-     provider_name="car_travel_direction",
-     elements=["Northbound", "Southbound"]
+    provider_name="car_travel_direction", elements=["Northbound", "Southbound"]
 )
 
-car_lane = DynamicProvider(
-     provider_name="car_lane",
-     elements=[1,2,3]
-)
+car_lane = DynamicProvider(provider_name="car_lane", elements=[1, 2, 3])
