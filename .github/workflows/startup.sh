0fd46d79af76ff46b3b96f5675defa3e312c8e9f
Luke Huntley <lahuntley@hotmail.com>
2023-03-11 21:57:20 +0800

    small changes

--- .github/workflows/startup.sh
+++
@@ -1,5 +0,0 @@
-# Read and set enviroment variables
-# export eval $(aws s3 cp s3://traffic-monitor-env-s3-bucket/.env - | sed -e 's/[\r\n]//g')
-
-# Start the producer
-python producer.py -f confluent.config -t traffic-monitor-topic -d 3
